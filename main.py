5e9c02b62bb94b9f4a3f785224cd12cd525f3229
Muhammad Waleed <77747037+blurryface92@users.noreply.github.com>
2022-06-03 15:37:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'info.locumforce@gmail.com'
-app.config['MAIL_PASSWORD'] = '919018##'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 
